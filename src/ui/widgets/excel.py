e6ac86ffb39b1e7bbb75960fa741188a6fed2b5d
amin-egn <aminx15am@yahoo.com>
2022-02-06 12:05:21 +0330

    fix some bugs and change design pattern

--- src/ui/widgets/excel.py
+++ src/ui/widgets/excel.py
@@ -1,6 +1,6 @@
 # internal
 from .base_dialog import BaseDialog
-from src.ui.component import FireButton, DrgDrpTable, CheckList, ListView
+from src.ui.component import FireButton, CheckList, ListView, SingleDimensionTableModel, TableView
 # pyqt
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QHBoxLayout
@@ -12,27 +12,41 @@ class ExcelResponse(BaseDialog):
         # self modification
         self.setWindowTitle('Excel Titles')
         self.setFixedWidth(320)
-        # excel model
+        # checklist model
         self.excelCheckListModel = CheckList()
-        # excel headers
-        self.listExcelHeaders = ListView(self.excelCheckListModel)
+        # list view
+        self.excelListView = ListView(self.excelCheckListModel)
+        # table model
+        self.excelTableModel = SingleDimensionTableModel([])
+        # table view
+        self.excelTableView = TableView(self.excelTableModel)
         # done button
         self.btnDoneLayout = QHBoxLayout()
         self.btnDoneLayout.setAlignment(Qt.AlignHCenter)
         self.btnDone = FireButton('Done')
         # attach
-        self.generalLayout.addWidget(self.listExcelHeaders)
+        # - button layout
         self.btnDoneLayout.addWidget(self.btnDone)
+        # - general
+        self.generalLayout.addWidget(self.excelListView)
         self.generalLayout.addLayout(self.btnDoneLayout)
+        # - entrance
+        self.ui.modelsLayout.addWidget(self.excelTableView)
 
     def _innerToMainTable(self):
         checkedItems = list()
+        try:
+            for i in sorted(self.excelCheckListModel.checkList):
+                checkedItems.append(self.excelCheckListModel.items[i])
 
-        for i in sorted(self.excelCheckListModel.checkList):
-            checkedItems.append(self.excelCheckListModel.items[i])
+            self.excelTableModel.header = ['Excel Headers']
 
-        modelDrgDrp = DrgDrpTable(['Excel Headers'], checkedItems)
-        self.ui.tableWidget.setModel(modelDrgDrp)
+        except Exception as e:
+            print(str(e))
+            self.excelTableModel.clearRecords()
+
+        finally:
+            self.excelTableModel.setRecords(checkedItems)
 
     # noinspection PyUnresolvedReferences
     def _connectSignals(self):
